452b15cc2c2712391752dba6ec2cf3d89183a931
Chen Wu <chenwu054@Chens-MacBook-Pro.local>
2016-06-23 10:43:07 -0700

    move a ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+//	void Start () {
+//	
+//	}
+	
+	// Update is called once per frame
+	// before rending a frame
+	private Rigidbody rb;
+	public float speed;
+
+	void Start() { // called when the first frame is active
+		rb = GetComponent<Rigidbody> (); // search the object, which this script is attached to, for a rigidbody 
+
+	}
+	void Update () {
+		
+	}
+	// before performing any physics update
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 v3 = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (v3 * speed);
+	
+	}
+}
