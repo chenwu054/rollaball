9a02652cfdc8efcc0d5cae4ab2fe5a9d52b9ea4b
Chen Wu <chenwu054@Chens-MacBook-Pro.local>
2016-06-23 19:16:29 -0700

    added rotating pickups

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () { // guaranteed to run after all objects have been processed and updated.  
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpController : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (10, 15, 30) * Time.deltaTime);
+	}
+}
